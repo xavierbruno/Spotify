fdf04b73d9e8742354cd0e221f301ff6966cd4d1
xavierbruno <brunovx6@gmail.com>
2025-05-15 02:46:40 -0300

    add route react

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import ItemList from './ItemList'
+import { artistArray } from '../assets/database/artists.js'
+import { songsArray } from '../assets/database/songs.js'
 
 const Main = () => {
   return (
     <div className='main'>
-      <ItemList />
-      <ItemList />
-      <ItemList />
-
+      <ItemList 
+        title="Artistas populares"
+        items ={5}
+        itemsArray={artistArray}
+        path="/artists"
+        idPath = "/artist"
+      />
+      <ItemList 
+        title="Músicas"
+        items={10}
+        itemsArray={songsArray}
+        path="/songs"
+        idPath = "/song"
+      />
     </div>
   )
 }
